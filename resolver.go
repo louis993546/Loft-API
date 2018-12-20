a7d124a49a26c7f0db5cee0ff4fd0abd96315eb6
Louis Tsai <louis993546@gmail.com>
2018-12-20 23:23:28 +0100

    ADD: resolver for adding task to a loft

--- resolver.go
+++ resolver.go
@@ -22,7 +22,33 @@ func (r *Resolver) Query() QueryResolver {
 type mutationResolver struct{ *Resolver }
 
 func (r *mutationResolver) CreateTask(ctx context.Context, input NewTask) (Task, error) {
-	panic("not implemented")
+	loftPos := sort.Search(len(r.lofts), func(i int) bool {
+		return r.lofts[i].ID == input.LoftID
+	})
+
+	if loftPos == len(r.lofts) {
+		var nothing Task
+		return nothing, fmt.Errorf("Cannot find loft #%s", input.LoftID)
+	}
+
+	newTask := Task{
+		ID:    "pretend this is a random ID",
+		Title: input.Title,
+		State: TaskStateNotDone,
+	}
+
+	thisLoft := r.lofts[loftPos]
+	updatedLoft := Loft{
+		ID:       thisLoft.ID,
+		Name:     thisLoft.Name,
+		Members:  thisLoft.Members,
+		Events:   thisLoft.Events,
+		Requests: thisLoft.Requests,
+		Tasks:    append(thisLoft.Tasks, newTask),
+	}
+	r.lofts[loftPos] = updatedLoft
+
+	return newTask, nil
 }
 func (r *mutationResolver) CreateEvent(ctx context.Context, input NewEvent) (Event, error) {
 	panic("not implemented")
