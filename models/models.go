5b8959d52abd017101e24fadafaf810dd009df23
Louis Tsai <louis993546@gmail.com>
2019-01-01 17:22:03 +0100

    ADD: gofrs/uuid

--- models/models.go
+++ models/models.go
@@ -1,12 +1,12 @@
 package models
 
+import "time"
+import "github.com/gofrs/uuid"
+
 // Loft is basically what the database can retrieve in a reasonable manner
-// TODO: I think even the count don't belong here. they probably should have their own resolvers
 type Loft struct {
-	ID            string //TODO: use uuid instead?
-	Name          string
-	MembersCount  int
-	TasksCount    int
-	EventsCount   int
-	RequestsCount int
+	ID        uuid.UUID
+	Name      string
+	JoinCode  string
+	CreatedAt time.Time
 }
